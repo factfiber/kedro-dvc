99a3f80396a5ff70ee2084023ebb518a33739694
Eli <fishman2028@gmail.com>
2022-02-13 14:11:41 -0700

    test: added dvc repo fixture

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,18 +1,5 @@
-# import os
-import subprocess
-
-# import dvc.api
-from pytest_cases import fixture
-
 import kedro_dvc
 
 
-@fixture(scope="session")  # put in different file fixtures.py
-def dvc_repo(tmpdir):
-    # os.mkdir("./temp_test")
-    # subprocess.Popen('dvc init --subdir', cwd="./temp_test")
-    subprocess.Popen("ls", cwd=tmpdir)
-
-
-def test_demo(dvc_repo):
-    kedro_dvc is not None
+def test_demo(dvc_repo_session):
+    assert kedro_dvc is not None
