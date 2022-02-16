d561d9741cb5f29464999ddd62927259df2e2df0
Eli <fishman2028@gmail.com>
2022-02-15 19:30:05 -0700

    fix: tests modifcations. Commit for pull

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,3 @@
-# import os
-# import shutil
 import subprocess
 import tempfile
 
@@ -10,13 +8,13 @@ from pytest_cases import fixture
 @fixture(name="dvc_repo_session", scope="session")
 def fix_dvc_repo_session() -> DvcRepo:
     with tempfile.TemporaryDirectory() as dir:
-        # os.mkdir("./temp_test")
         subprocess.check_call(["git", "init"], cwd=dir)
+        print(dir)
         dvc = DvcRepo.init(dir, subdir=True)
         yield dvc
         dvc.close()
 
 
 @fixture(name="dvc_repo")
-def fix_dvc_repo() -> DvcRepo:
-    return fix_dvc_repo_session()
+def fix_dvc_repo(dvc_repo_session) -> DvcRepo:
+    return  # take the repo from^ this and make a copy
