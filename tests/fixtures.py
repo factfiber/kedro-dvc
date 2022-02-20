7eeb25cd40216ef63b2f8c3b3e9213344eaf7e65
Eli <fishman2028@gmail.com>
2022-02-20 09:59:09 -0700

    fix: non-session fixtures now create their own tempdir

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -11,11 +11,10 @@ from pytest_cases import fixture
 @fixture(name="dvc_repo_session", scope="session")
 def fix_dvc_repo_session() -> DvcRepo:
     with tempfile.TemporaryDirectory() as dir:
-        this_dir = dir+"/fix_dvc_repo_session"
-        subprocess.check_call(["git", "init"], cwd=this_dir)
-        dvc = DvcRepo.init(this_dir, subdir=True)
+        subprocess.check_call(["git", "init"], cwd=dir)
+        dvc = DvcRepo.init(dir, subdir=True)
         prev_dir = os.getcwd()
-        os.chdir(this_dir)
+        os.chdir(dir)
 
         yield dvc
 
@@ -24,20 +23,20 @@ def fix_dvc_repo_session() -> DvcRepo:
 
 @fixture(name="dvc_repo")
 def fix_dvc_repo(dvc_repo_session) -> DvcRepo:
-    dvc_repo = copy.deepcopy(dvc_repo_session)
-    dvc_repo.root_dir += "_dvc_repo"
-    shutil.copytree(dvc_repo_session.root_dir, dvc_repo.root_dir)
-    prev_dir = os.getcwd()
-    os.chdir(dvc_repo.root_dir)
+    with tempfile.TemporaryDirectory() as dir:
+        dvc_repo = copy.deepcopy(dvc_repo_session)
+        dvc_repo.root_dir = dir
+        shutil.copytree(dvc_repo_session.root_dir, dvc_repo.root_dir)
+        prev_dir = os.getcwd()
+        os.chdir(dvc_repo.root_dir)
 
-    yield dvc_repo
-    
-    os.chdir(prev_dir)
+        yield dvc_repo
+        
+        os.chdir(prev_dir)
 
 @fixture(name="empty_repo_session", scope="session")
 def fix_empty_repo_session() -> str:
     with tempfile.TemporaryDirectory() as dir:
-        this_dir = dir+"/empty_repo_session"
         # Might be able to do this from python, not sure
         subprocess.check_call(
             f"poetry exec create-sample-project test-project sample-project-basic", 
@@ -47,9 +46,9 @@ def fix_empty_repo_session() -> str:
             f"mv tmp/name {dir}/tmp/name",
             shell=True
         )
-        subprocess.check_call(["git", "init"], cwd=this_dir)
+        subprocess.check_call(["git", "init"], cwd=dir)
         prev_dir = os.getcwd()
-        os.chdir(this_dir)
+        os.chdir(dir)
 
         yield dir
 
@@ -57,13 +56,14 @@ def fix_empty_repo_session() -> str:
 
 @fixture(name="empty_repo_session")
 def fix_empty_repo(empty_repo_session) -> str:
-    empty_repo = empty_repo_session + "_emtpy_repo"
-    shutil.copytree(empty_repo_session, empty_repo)
-    prev_dir = os.getcwd()
-    os.chdir(empty_repo)
+    with tempfile.TemporaryDirectory() as dir:
+        empty_repo = dir
+        shutil.copytree(empty_repo_session, empty_repo)
+        prev_dir = os.getcwd()
+        os.chdir(empty_repo)
 
-    yield empty_repo
+        yield empty_repo
 
-    os.chdir(prev_dir)
+        os.chdir(prev_dir)
 
 
