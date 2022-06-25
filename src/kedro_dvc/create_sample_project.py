d168e390c3303c8c839f6bf6a49bf72951d84153
Shaun Cutts <shaunc@factfiber.com>
2022-06-25 15:48:03 -0400

    fix: rm duplicate .git; configure git user to dummy if nec.

--- src/kedro_dvc/create_sample_project.py
+++ src/kedro_dvc/create_sample_project.py
@@ -35,13 +35,11 @@ def _default_initial_commit() -> None:
     Initial commit with sample project skeleton.
     """
     subprocess.check_call(["git", "add", "."])
-    user_out = subprocess.check_output(["git" "config" "user.name"]).decode()
+    user_out = subprocess.check_output(["git", "config", "user.name"]).decode()
     if user_out.strip() == "":
+        subprocess.check_call(["git", "config", "user.name", "dummy"])
         subprocess.check_call(
-            ["git", "config", "user.name", "dummy"], shell=True
-        )
-        subprocess.check_call(
-            ["git", "config", "user.email", "dummy@factfiber.com"], shell=True
+            ["git", "config", "user.email", "dummy@factfiber.com"]
         )
     subprocess.check_call(["git", "commit", "-m", "chore: initial commit"])
 
